adf0c9e2f06b52dae7d2089d0513bd6ceff83322
Cari_mf <Carifer_411@users.noreply.github.com>
2020-08-22 20:17:59 -0300

    Add clas FillNulls

--- my_custom_sklearn_transforms/sklearn_transformers.py
+++ my_custom_sklearn_transforms/sklearn_transformers.py
@@ -1,5 +1,12 @@
 from sklearn.base import BaseEstimator, TransformerMixin
 
+import logging
+logging.basicConfig(
+    level=logging.INFO,
+    format='%(asctime)s-%(name)s-%(levelname)s: %(message)s',
+    handlers=[logging.FileHandler('btc-desafio2.log'), logging.StreamHandler()],
+)
+logger = logging.getLogger(__name__)
 
 # All sklearn Transforms must have the `transform` and `fit` methods
 class DropColumns(BaseEstimator, TransformerMixin):
@@ -11,6 +18,52 @@ class DropColumns(BaseEstimator, TransformerMixin):
 
     def transform(self, X):
         # Primeiro realizamos a cópia do dataframe 'X' de entrada
+        logger.info(
+        f"Dropping the following {len(self.columns)} unusable columns:\n"
+        f"{self.columns}"
+        )
+
         data = X.copy()
+
         # Retornamos um novo dataframe sem as colunas indesejadas
-        return data.drop(labels=self.columns, axis='columns')
+        df = data.drop(labels=self.columns, axis='columns')
+        
+        logger.info(
+        f"Remaining {len(df.columns)} columns:\n {sorted(df.columns.tolist())}"
+        )
+        return df
+
+class FillNulls(BaseEstimator, TransformerMixin):
+    def  __init__(self, columns):
+        self.columns = columns
+        
+    def fit(self, X, y=None):
+        return self
+        
+    #define el tipo de la columna
+    def _get_typed_cols(self, col_type ='cat'): 
+        assert col_type in ('cat', 'num')
+        include = 'object' if col_type == 'cat' else [np.number]
+        typed_cols = [
+            c for c in self.select_dtypes(include=include).columns
+        ]
+        return typed_cols
+
+    #completa los nulos con valores promedio
+    def _fill_nulls(self): 
+        data = self.copy()
+        for t in ['num', 'cat']:
+            cols = _get_typed_cols(data, col_type=t)
+            for c in cols:
+                if t == 'num':
+                    data[c] = data[c].fillna(data[c].median())
+                else:
+                    val_count = data[c].value_counts(dropna=True)
+                    common_val = val_count.index.tolist()[0]
+                    data[c] = data[c].fillna(common_val)
+        return data
+    
+    def transform(self, X):
+        data = X.copy()
+        df = _fill_nulls(data)
+        return df
